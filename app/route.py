1d148eac7becbe32d6cdfcbbd30eb5d948c7a4ed
root <root@367d4e85c1d9>
2021-11-04 11:56:49 +0000

    20211104

--- app/route.py
+++ app/route.py
@@ -33,7 +33,16 @@ def get_data_from_pgdb(pgdb_config,sqls):
 
 # 頁面類
 def index():
-    return render_template('index.html')
+    sqls = '''SELECT stock_no, stock_name, price, ROUND( dividend::numeric, 2 ) as dividend, yield
+	FROM public.stock_list_stats
+	where per < 20
+	and pbr <2
+	and l10y_div_cnt >5
+	and (five_line_2y like '%標準差之外相對低點%' or five_line_1y like '%標準差之外相對低點%')
+	and stock_no in ('2330','2454',	'2317',	'2303',	'2881',	'2308',	'1301',	'1303',	'2882',	'2412',	'2891',	'2002',	'2886',	'5871',	'2884',	'3711',	'1216',	'6415',	'2885',	'2603',	'1326',	'1101',	'2357',	'2892',	'2379',	'3034',	'5880',	'2382',	'3008',	'2880',	'2615',	'2887',	'2207',	'2327',	'2409',	'2609',	'2912',	'3045',	'2395',	'5876',	'4938',	'6505',	'1590',	'1402',	'2801',	'8046',	'8454',	'4904',	'9910',	'2408')
+	order by yield desc'''
+    recommed_tstock = get_data_from_pgdb(pgdb_config,sqls)
+    return render_template('index.html', recommed_tstock=recommed_tstock)
 
 def stock_list():
     stock_list_stats_sql = '''SELECT * FROM stock_list_stats;'''
